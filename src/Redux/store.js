dd807bf002cf72ca579b681a47aef37c8026c5ca
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2024-01-19 23:04:54 +0100

    persist

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,43 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { autosReducer } from './Autos/autosSlice';
-import { filterReducer } from './Autos/filterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { autosReducer } from './Autos/autosSlice';
+// import { filterReducer } from './Autos/filterSlice';
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
-//   const authPersistConfig = {
-//     key: 'auth',
-//     storage,
-//     whitelist: ['token'],
-//   };
+// //   const authPersistConfig = {
+// //     key: 'auth',
+// //     storage,
+// //     whitelist: ['token'],
+// //   };
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
 
-export const store = configureStore({
-  reducer: {
-    //   auth: persistReducer(authPersistConfig, authReducer),
-    autos: persistReducer(persistConfig, autosReducer),
-    filter: filterReducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-      devTools: process.env.NODE_ENV === 'development',
-    }),
-});
+// const persistedReducer = persistReducer(persistConfig, autosReducer);
 
-export const persistor = persistStore(store);
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   // {
+//   //   //   auth: persistReducer(authPersistConfig, authReducer),
+//   //   autos: persistReducer(persistConfig, autosReducer),
+//   //   filter: filterReducer,
+//   // },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//       devTools: process.env.NODE_ENV === 'development',
+//     }),
+// });
+
+// export const persistor = persistStore(store);

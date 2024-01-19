d298eb4f543f06d7ada2b394a8431bda4dc09de1
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2024-01-20 00:26:43 +0100

    rout2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, lazy, Suspense } from 'react';
 import { Layout } from './Layout';
@@ -14,17 +14,21 @@ import FavoritesPage from '../pages/FavoritesPage';
 
 export const App = () => {
   return (
-    <Router>
-      <Suspense>
-        <Layout>
+    <>
+      <BrowserRouter>
+        <Suspense>
+          {/* <Layout> */}
           <Routes>
+            {/* <Route path="/" element={<Layout />}> */}
             <Route path="/" element={<HomePage />} />
             <Route path="/catalog" element={<CatalogPage />} />
             <Route path="/favorites" element={<FavoritesPage />} />
             {/* <Route path="*" element={<Navigate to="/" />} /> */}
+            {/* </Route> */}
           </Routes>
-        </Layout>
-      </Suspense>
-    </Router>
+          {/* </Layout> */}
+        </Suspense>
+      </BrowserRouter>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 // import { Provider } from 'react-redux';
